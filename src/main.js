429ae95ab38f7e13bd1ab4cf339a867865f2fa2e
kaiqingkqkq <xkaiqingkqkq@163.com>
2026-06-10 14:19:02 +0800

    将代码块更新为黑色

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'highlight.js/styles/github.css.css'
+import 'highlight.js/styles/github-dark.css'
 
 createApp(App).mount('#app')
